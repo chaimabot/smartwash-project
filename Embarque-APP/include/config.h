8406d71a2ce10645757fa87714a71af463ad6f25
hajhamadisraa <israahajhamad272@gmail.com>
2025-11-22 17:43:13 +0100

    Ajout du dossier Embarque-APP comme dossier normal

---
+++ Embarque-APP/include/config.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <Arduino.h>
+
+// Wi-Fi
+
+#define WIFI_SSID "IsraaPhone"
+#define WIFI_PASSWORD "01020304"
+
+// MQTT
+#define MQTT_SERVER "172.20.10.3"
+#define MQTT_PORT 1883
+
+// LED pour simuler la machine
+#define LED_PIN 2
